6cb5bc1bd7ed69613be32a2e8c61870575e517f3
thiefyzheng <thiefyzheng@gmail.com>
2023-05-17 14:20:18 +0800

    Fixed votespam/

--- comments.py
+++ comments.py
@@ -56,19 +56,27 @@ def upvote_comment(user_id, comment_id):
         print("Upvote removed successfully")
     else:
         # The user has not upvoted this comment yet
-        # Increment the upvotes count for the comment
-        query = "UPDATE comments SET upvotes=upvotes+1 WHERE id=%s"
-        cursor.execute(query, (comment_id,))
-        # Insert a record in the comment_votes table to track that the user has upvoted this comment
-        query = "INSERT INTO comment_votes (user_id, comment_id, vote) VALUES (%s, %s, 1)"
+        # Check if the user has already downvoted this comment
+        query = "SELECT * FROM comment_votes WHERE user_id=%s AND comment_id=%s AND vote=-1"
         cursor.execute(query, (user_id, comment_id))
-        print("Upvoted successfully")
+        result = cursor.fetchall()
+        if result:
+            # The user has already downvoted this comment
+            # Do not allow the user to upvote and downvote at the same time
+            print("Cannot upvote and downvote at the same time")
+        else:
+            # The user has not downvoted this comment yet
+            # Increment the upvotes count for the comment
+            query = "UPDATE comments SET upvotes=upvotes+1 WHERE id=%s"
+            cursor.execute(query, (comment_id,))
+            # Insert a record in the comment_votes table to track that the user has upvoted this comment
+            query = "INSERT INTO comment_votes (user_id, comment_id, vote) VALUES (%s, %s, 1)"
+            cursor.execute(query, (user_id, comment_id))
+            print("Upvoted successfully")
 
     # Commit the changes and close the database connection
     conn.commit()
     conn.close()
-
-
 
 
 def downvote_comment(user_id, comment_id):
@@ -91,13 +99,23 @@ def downvote_comment(user_id, comment_id):
         print("Downvote removed successfully")
     else:
         # The user has not downvoted this comment yet
-        # Increment the downvotes count for the comment
-        query = "UPDATE comments SET downvotes=downvotes+1 WHERE id=%s"
-        cursor.execute(query, (comment_id,))
-        # Insert a record in the comment_votes table to track that the user has downvoted this comment
-        query = "INSERT INTO comment_votes (user_id, comment_id, vote) VALUES (%s, %s, -1)"
+        # Check if the user has already upvoted this comment
+        query = "SELECT * FROM comment_votes WHERE user_id=%s AND comment_id=%s AND vote=1"
         cursor.execute(query, (user_id, comment_id))
-        print("Downvoted successfully")
+        result = cursor.fetchall()
+        if result:
+            # The user has already upvoted this comment
+            # Do not allow the user to upvote and downvote at the same time
+            print("Cannot upvote and downvote at the same time")
+        else:
+            # The user has not upvoted this comment yet
+            # Increment the downvotes count for the comment
+            query = "UPDATE comments SET downvotes=downvotes+1 WHERE id=%s"
+            cursor.execute(query, (comment_id,))
+            # Insert a record in the comment_votes table to track that the user has downvoted this comment
+            query = "INSERT INTO comment_votes (user_id, comment_id, vote) VALUES (%s, %s, -1)"
+            cursor.execute(query, (user_id, comment_id))
+            print("Downvoted successfully")
 
     # Commit the changes and close the database connection
     conn.commit()
