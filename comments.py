5515127c62fb0cb25cd7015480d04a1bf0126bfa
thiefyzheng <thiefyzheng@gmail.com>
2023-05-17 11:15:49 +0800

    fixed upvotes and downvotes causing the server error also now you can remove your votessss

--- comments.py
+++ comments.py
@@ -44,9 +44,16 @@ def upvote_comment(user_id, comment_id):
     # Check if the user has already upvoted this comment
     query = "SELECT * FROM comment_votes WHERE user_id=%s AND comment_id=%s"
     cursor.execute(query, (user_id, comment_id))
-    if cursor.fetchone():
+    result = cursor.fetchall()
+    if result:
         # The user has already upvoted this comment
-        print("You have already upvoted this comment")
+        # Decrement the upvotes count for the comment
+        query = "UPDATE comments SET upvotes=upvotes-1 WHERE id=%s"
+        cursor.execute(query, (comment_id,))
+        # Delete the record in the comment_votes table to track that the user has removed their upvote
+        query = "DELETE FROM comment_votes WHERE user_id=%s AND comment_id=%s"
+        cursor.execute(query, (user_id, comment_id))
+        print("Upvote removed successfully")
     else:
         # The user has not upvoted this comment yet
         # Increment the upvotes count for the comment
@@ -62,6 +69,8 @@ def upvote_comment(user_id, comment_id):
     conn.close()
 
 
+
+
 def downvote_comment(user_id, comment_id):
     # Connect to the database
     conn = mysql.connector.connect(host=db_host, user=db_user, password=db_password, database=db_name)
@@ -70,9 +79,16 @@ def downvote_comment(user_id, comment_id):
     # Check if the user has already downvoted this comment
     query = "SELECT * FROM comment_votes WHERE user_id=%s AND comment_id=%s AND vote=-1"
     cursor.execute(query, (user_id, comment_id))
-    if cursor.fetchone():
+    result = cursor.fetchall()
+    if result:
         # The user has already downvoted this comment
-        print("You have already downvoted this comment")
+        # Decrement the downvotes count for the comment
+        query = "UPDATE comments SET downvotes=downvotes-1 WHERE id=%s"
+        cursor.execute(query, (comment_id,))
+        # Delete the record in the comment_votes table to track that the user has removed their downvote
+        query = "DELETE FROM comment_votes WHERE user_id=%s AND comment_id=%s AND vote=-1"
+        cursor.execute(query, (user_id, comment_id))
+        print("Downvote removed successfully")
     else:
         # The user has not downvoted this comment yet
         # Increment the downvotes count for the comment
