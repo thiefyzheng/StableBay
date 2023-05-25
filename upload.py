10392a8e9012063bb13e49f53338fb976714b659
thiefyzheng <thiefyzheng@gmail.com>
2023-05-25 08:54:09 +0800

    allowed blur and unblur of nsfw

--- upload.py
+++ upload.py
@@ -3,12 +3,12 @@ import datetime
 import uuid
 import mysql.connector
 from config import db_password
+
 app = Flask(__name__)
 
 # Database configuration
 db_host = 'localhost'
 db_user = 'stablebay'
-
 db_name = 'StableDB'
 
 # Function to execute queries on MySQL
@@ -29,16 +29,16 @@ def execute_query(query, params=None, fetchall=False):
 
 # Create models table if it doesn't exist yet
 execute_query('''CREATE TABLE IF NOT EXISTS models (
- id VARCHAR(66) NOT NULL PRIMARY KEY,
- name VARCHAR(255) NOT NULL,
- description VARCHAR(255),
- magnet_link VARCHAR(10000) NOT NULL,
- image_link VARCHAR(255),
- uploaded_by VARCHAR(255) NOT NULL,
- upload_date DATETIME NOT NULL,
- category INT,
- nsfw BOOLEAN DEFAULT FALSE
- )''')
+    id VARCHAR(66) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    magnet_link VARCHAR(10000) NOT NULL,
+    image_link VARCHAR(255),
+    uploaded_by VARCHAR(255) NOT NULL,
+    upload_date DATETIME NOT NULL,
+    category INT,
+    nsfw BOOLEAN DEFAULT FALSE
+)''')
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -62,19 +62,24 @@ def upload():
         short_description = request.form['short_description']
         magnet_link = request.form['magnet_link']
         image_link = request.form['image_link']
+        nsfw = 1 if request.form.get('nsfw') else 0
+
 
         # Generate unique ID
         model_id = str(uuid.uuid4())
 
         # create dictionary for model info
-        model_info = {'id': model_id, 'name': model_name, 'description': short_description, 'magnet_link': magnet_link, 'image_link': image_link,
-                      'uploaded_by': session.get('username'), 'upload_date': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+        model_info = {'id': model_id, 'name': model_name, 'description': short_description, 'magnet_link': magnet_link,
+                      'image_link': image_link,
+                      'uploaded_by': session.get('username'),
+                      'upload_date': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
 
         # Insert model metadata into database
-        query = "INSERT INTO models (id, name, description, magnet_link, image_link, uploaded_by, upload_date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        params = (model_info['id'], model_info['name'], model_info['description'], model_info['magnet_link'], model_info['image_link'],
-                  model_info['uploaded_by'], model_info['upload_date'])
-        execute_query(query, params)
+        query = "INSERT INTO models (id, name, description, magnet_link, image_link, uploaded_by, upload_date, nsfw) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+        params = (model_info['id'], model_info['name'], model_info['description'], model_info['magnet_link'],
+                  model_info['image_link'],
+                  model_info['uploaded_by'], model_info['upload_date'], nsfw)
+        execute_query(query,params)
 
         return redirect(url_for('upload2', model_id=model_id))
 
