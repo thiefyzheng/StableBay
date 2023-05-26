55662764b4c9195808ccdb7b52aadd810c2b3b78
thiefyzheng <thiefyzheng@gmail.com>
2023-05-26 10:12:43 +0800

    added file types

--- database.py
+++ database.py
@@ -136,10 +136,11 @@ cursor.execute('''
 
 # Insert some sample attributes
 attributes = [('Training Data', 'The type of training data used'),
-              ('Merge', 'Whether the model merges information'),
-              ('Model Type', 'The type of model used, e.g. realism, anime, etc.'),
-              ('LoRA Type', 'The type of LoRA model used'),
-              ('Trigger Words', 'The trigger words used by the model')]
+ ('Merge', 'Whether the model merges information'),
+ ('Model Type', 'The type of model used, e.g. realism, anime, etc.'),
+ ('LoRA Type', 'The type of LoRA model used'),
+ ('Trigger Words', 'The trigger words used by the model'),
+ ('File Type', 'The type of file used by the model')]
 
 for attribute in attributes:
     query = "INSERT INTO attributes (name, description) SELECT %s, %s WHERE NOT EXISTS (SELECT * FROM attributes WHERE name=%s)"
@@ -147,16 +148,16 @@ for attribute in attributes:
 
 # Define the category-attribute relationships
 category_attributes = [('Checkpoint', 'Training Data', True),
-                       ('Checkpoint', 'Merge', True),
-                       ('Checkpoint', 'Model Type', True),
-                       ('LoRA', 'LoRA Type', True),
-                       ('LoRA', 'Training Data', True),
-                       ('LoRA', 'Trigger Words', True),
-                       ('Textual Inversion', 'Training Data', True),
-                       ('Textual Inversion', 'Trigger Words', True),
-                       ('Hypernetwork', 'Training Data', True),
-                       ('Hypernetwork', 'Trigger Words', True),
-                       ('Controlnet', 'Training Data', True)]
+ ('Checkpoint', 'Merge', True),
+ ('Checkpoint', 'Model Type', True),
+ ('LoRA', 'LoRA Type', True),
+ ('LoRA', 'Training Data', True),
+ ('LoRA', 'Trigger Words', True),
+ ('Textual Inversion', 'Training Data', True),
+ ('Textual Inversion', 'Trigger Words', True),
+ ('Hypernetwork', 'Training Data', True),
+ ('Hypernetwork', 'Trigger Words', True),
+ ('Controlnet', 'Training Data', True)]
 
 for category_attribute in category_attributes:
     category_query = "SELECT id FROM categories WHERE name=%s"
@@ -174,6 +175,18 @@ for category_attribute in category_attributes:
         # Insert the category-attribute relationship if it doesn't exist
         query = "INSERT INTO category_attributes (category_id, attribute_id, is_required) VALUES (%s, %s, %s)"
         cursor.execute(query, (category_id, attribute_id, category_attribute[2]))
+
+# Get the ID of the File Type attribute
+attribute_query = "SELECT id FROM attributes WHERE name='File Type'"
+cursor.execute(attribute_query)
+file_type_attribute_id = cursor.fetchone()[0]
+
+# Link every category with the File Type attribute
+category_query = "SELECT id FROM categories"
+cursor.execute(category_query)
+for category_id in cursor.fetchall():
+    query = "INSERT INTO category_attributes (category_id, attribute_id) VALUES (%s, %s)"
+    cursor.execute(query, (category_id[0], file_type_attribute_id))
 
 # Commit the changes and close the database connection
 conn.commit()
